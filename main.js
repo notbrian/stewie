4d5083014411312bb1311ca31d1963220cfe022d
Brian Nguyen <nguyen.brian70@gmail.com>
2017-11-04 01:44:59 -0400

    Add texting code

--- main.js
+++ main.js
@@ -4,10 +4,9 @@ var authToken = '9de0366e2ae88807ad3e74a1a8c6ad64';   // Your Auth Token from ww
 var twilio = require('twilio');
 var client = new twilio(accountSid, authToken);
 
-client.api.calls
-  .create({
-    url: 'http://demo.twilio.com/docs/voice.xml',
-    to: '+14169049147',
-    from: '+16474960670',
-  })
-  .then((call) => console.log(call.sid));
+client.messages.create({
+    body: 'Hello from Node',
+    to: '+14169049147',  // Text this number
+    from: '+16474960670' // From a valid Twilio number
+})
+.then((message) => console.log(message.sid));
