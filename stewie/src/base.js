d7debdafd433c5d6a8b904bc06dac5073a0f0314
Brian Nguyen <nguyen.brian70@gmail.com>
2017-12-22 17:32:20 -0500

    Renamed frontend

--- stewie/src/base.js
+++
@@ -1,9 +0,0 @@
-import Rebase from 're-base';
-
-const base = Rebase.createClass({
-  apiKey: "AIzaSyCyTpihMAroNqReFOErgq210gJdU2fYU9Y",
-  authDomain: "stewie-data.firebaseapp.com",
-  databaseURL: "https://stewie-data.firebaseio.com",
-});
-
-export default base;
